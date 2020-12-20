c6742e3c62033c30f66b8e88c76732eae6dde4e9
PengNi <543943952@qq.com>
2020-12-20 15:48:12 +0800

    scripts to count model params

---
+++ tests/model_parameters.py
@@ -0,0 +1,15 @@
+from prettytable import PrettyTable
+
+def count_parameters(model):
+    table = PrettyTable(["Modules", "Parameters"])
+    total_params = 0
+    for name, parameter in model.named_parameters():
+        if not parameter.requires_grad: continue
+        param = parameter.numel()
+        table.add_row([name, param])
+        total_params+=param
+    print(table)
+    print(f"Total Trainable Params: {total_params}")
+    return total_params
+    
+count_parameters(net)
